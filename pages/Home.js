7d67fe71b8494220ce1b890615c280ea5dcd550f
aryan basu <aryanbasu005@gmail.com>
2024-01-16 14:25:48 +0530

    minor changes

--- pages/Home.js
+++ pages/Home.js
@@ -9,9 +9,11 @@ const Home = () => {
     //search functionality
     const filterBySearch = (event) => {
         // Access input value
+
         const query = event.target.value;
         // Create copy of item list
-        let updatedList = [...Data];
+        let updatedList = Data.filter(function (obj) { return SelectList.indexOf(obj) === -1; });
+
         // Include all elements which includes the search query
         updatedList = updatedList.filter((item) => {
             return item.name.toString().toLowerCase().indexOf(query.toLowerCase()) !== -1;
@@ -19,19 +21,26 @@ const Home = () => {
         // Trigger render with updated values
         setFilteredList(updatedList);
     };
-    const handleselecteditem = (index) => {
+    const handleselecteditem = (index, e) => {
 
         let arr = []
         SelectList.forEach((data) => {
             arr.push(data)
         })
         arr.push(filteredList[index])
+
         filteredList.splice(index, 1);
+
         setSelectedList(arr)
 
         setdisplay(false)
         //clearing the input text
         document.getElementById('search').value = ''
+        let updatedList = Data.filter(function (obj) { return SelectList.indexOf(obj) === -1; });
+        // Trigger render with updated values
+        setFilteredList(updatedList);
+        // e.target.value=''
+        // filterBySearch(e)
 
     }
     const handleunselecteditem = (index) => {
@@ -49,13 +58,42 @@ const Home = () => {
 
         //clearing the input text
         document.getElementById('search').value = ''
+
+        let List = Data.filter(function (obj) { return SelectList.indexOf(obj) === -1; });
+        // Trigger render with updated values
+        setFilteredList(List);
+    }
+    let count;
+    const handleKeyDown = event => {
+
+
+        // console.log(message);
+
+        if (event.key === 'Backspace' && document.getElementById('search').value === '') {
+            // 👇️ your logic here
+
+            count = count + 1;
+            if (count === 1) {
+
+            }
+            else if (count === 2) {
+
+            }
+            else {
+                count = 1;
+            }
+            console.log('Backspace key pressed ✅');
+        }
+        else {
+            count = 0;
+        }
     }
     useEffect(() => {
 
     }, [filteredList, SelectList])
     return (
         <div class="container mx-auto my-auto">
-            <div class=" justify-center items-center my-10">
+            <div class=" justify-center items-center">
 
                 <div class="relative flex">
                     {SelectList.map((data, index) => {
@@ -70,20 +108,30 @@ const Home = () => {
                         )
                     })}
 
-                    <input id="search" onClick={(e) => setdisplay(true)} onChange={filterBySearch} type="text" class="w-full  text-gray-700 rounded-md py-2 pl-4 pr-3 sm:text-sm focus:outline-none " placeholder="" />
+                    <input id="search"
+                        onClick={(e) => setdisplay(true)} onChange={filterBySearch} onKeyDown={(e) => { handleKeyDown(e) }} type="text" class="w-full  text-gray-700 rounded-md py-2 pl-4 pr-3 sm:text-sm focus:outline-none " placeholder="" />
 
                 </div>
                 <div class="h-1 bg-blue-500 w-full"></div>
                 <ul style={{ display: display ? "" : "none" }} class="list-disc ml-2">
                     {filteredList.map((data, index) => {
                         return (
-                            <li style={{ cursor: "pointer" }} onClick={(e) => { handleselecteditem(index, e) }} class=" wl-full border border-transparent flex hover:bg-gray-200 items-center py-4 space-x-4 px-4">
-                                <img class="w-16 h-16 rounded-full bg-gradient-to-br from-teal-400 to-blue-500" src={data.imgURL} alt="Avatar" />
-                                <div class="flex flex-row space-x-5">
-                                    <span class="text-left text-gray-700 font-medium">{data.name}</span>
-                                    <span class="text-right text-gray-500 text-sm mt-0.5">{data.email}</span>
-                                </div>
-                            </li>
+                            <>
+                                {index === 0 ? <li style={{ cursor: "pointer" }} onClick={(e) => { handleselecteditem(index, e) }} class=" wl-full border border-transparent flex bg-gray-200 items-center py-4 space-x-4 px-4">
+                                    <img class="w-16 h-16 rounded-full bg-gradient-to-br from-teal-400 to-blue-500" src={data.imgURL} alt="Avatar" />
+                                    <div class="flex flex-row space-x-5">
+                                        <span class="text-left text-gray-700 font-medium">{data.name}</span>
+                                        <span class="text-right text-gray-500 text-sm mt-0.5">{data.email}</span>
+                                    </div>
+                                </li> : <li style={{ cursor: "pointer" }} onClick={(e) => { handleselecteditem(index, e) }} class=" wl-full border border-transparent flex hover:bg-gray-200 items-center py-4 space-x-4 px-4">
+                                    <img class="w-16 h-16 rounded-full bg-gradient-to-br from-teal-400 to-blue-500" src={data.imgURL} alt="Avatar" />
+                                    <div class="flex flex-row space-x-5">
+                                        <span class="text-left text-gray-700 font-medium">{data.name}</span>
+                                        <span class="text-right text-gray-500 text-sm mt-0.5">{data.email}</span>
+                                    </div>
+                                </li>}
+                            </>
+
                         )
                     })}
                 </ul>
